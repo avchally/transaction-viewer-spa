ee4c685cb0f80203c0cb00bd4a81fbaeb72e74a4
Alex <vchallyalex@gmail.com>
2022-09-06 19:46:11 -0500

    feat: created foundation for frontend with graphql support
    Setup the start of the frontend, utilizing Nuxt/Vue 2 with
    TailwindCSS and accessing GraphQL endpoints through a NuxtJS
    wrapper of vue-apollo

---
+++ frontend/components/DataExtractTest.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="tran in transactions" :key="tran.id">
+        {{ `${tran.reference}: ${tran.amount} ${tran.currency}` }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from 'graphql-tag';
+
+export default {
+  name: 'DataExtractTestPage',
+
+  apollo: {
+    transactions: {
+      query: gql`query Query {
+        transactions {
+          id
+          account {
+            name
+            bank
+          }
+          category {
+            name
+            color
+          }
+          reference
+          amount
+          currency
+          date
+        }
+      }`,
+    }
+  },
+
+  components: {},
+
+  data() {
+    return {
+      transactions: [
+        {
+          id: '12341235',
+          reference: 'A reference',
+          amount: '-1234.50',
+          currency: 'GBP',
+        },
+        {
+          id: '54983542',
+          reference: 'Something else',
+          amount: '100.00',
+          currency: 'EUR',
+        },
+      ],
+    }
+  }
+}
+
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <DataExtractTest />
+</template>
+
+<script>
+import DataExtractTest from '../components/DataExtractTest.vue';
+export default {
+    name: "IndexPage",
+    components: { DataExtractTest }
+}
+</script>
